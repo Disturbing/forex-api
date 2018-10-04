91dfe4f05df4d44121062e99a189c9bd248d544b
ibeckermayer <ibeckermayer@gmail.com>
2018-10-03 20:11:09 -0700

    changed a lot of from's to frm's, removed default value for date which can't be specified programatically, implemented get_rate_frm_to

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -8,6 +8,16 @@ from swagger_server.models.iso4217s import ISO4217s  # noqa: E501
 from swagger_server import util
 
 from forex_python.converter import CurrencyRates
+from datetime import datetime
+
+# get a list of all currencies to check for errors
+all_curs = ["AUD"]
+c = CurrencyRates()
+for cur in c.get_rates("AUD"):
+    all_curs.append(cur)
+
+# Set the appropriate format string for parsing dates
+date_format = "%Y-%m-%d"
 
 def get_available_currencies():  # noqa: E501
     """Get an array of available currencies
@@ -17,20 +27,16 @@ def get_available_currencies():  # noqa: E501
 
     :rtype: ISO4217s
     """
-    all_curs = ["AUD"]
-    c = CurrencyRates()
-    for cur in c.get_rates('AUD'):
-        all_curs.append(cur)
-    return ISO4217s(all_curs)
+    return ISO4217s(all_curs), 200
 
 
-def get_convert_from_to_amt(_from, to, amt, date=None):  # noqa: E501
+def get_convert_frm_to_amt(_frm, to, amt, date=None):  # noqa: E501
     """gives converted amount based on parameters
 
     returns a Conversion object based on parameters # noqa: E501
 
-    :param _from: Three letter ISO 4217 alphabetic code for the currency thats being converted from.
-    :type _from: str
+    :param _frm: Three letter ISO 4217 alphabetic code for the currency thats being converted from.
+    :type _frm: str
     :param to: Three letter ISO 4217 alphabetic code for the currency thats being converted to.
     :type to: str
     :param amt: The amount of base currency to convert from.
@@ -43,13 +49,13 @@ def get_convert_from_to_amt(_from, to, amt, date=None):  # noqa: E501
     return 'do some magic!'
 
 
-def get_get_rate_from_to(_from, to, date=None):  # noqa: E501
+def get_get_rate_frm_to(frm, to, date=None):  # noqa: E501
     """Gets rate from one currency to another.
 
     Returns a Conversion object with from_amt equal to 1. # noqa: E501
 
-    :param _from: Three letter ISO 4217 alphabetic code for the currency that&#39;s being converted from.
-    :type _from: str
+    :param _frm: Three letter ISO 4217 alphabetic code for the currency that&#39;s being converted from.
+    :type _frm: str
     :param to: Three letter ISO 4217 alphabetic code for the currency that&#39;s being converted to.
     :type to: str
     :param date: YYYY-MM-DD format of date to check rates on
@@ -57,16 +63,30 @@ def get_get_rate_from_to(_from, to, date=None):  # noqa: E501
 
     :rtype: Conversion
     """
-    return 'do some magic!'
+    if frm not in all_curs:
+        return Error("404", "from parameter is not a supported ISO4217 alphabetic code"), 404
+    if to not in all_curs:
+        return Error("404", "to parameter is not a supported ISO4217 alphabetic code"), 404
+
+    if date is None:
+        date = datetime.today().strftime(date_format)
+
+    try:
+        dt = datetime.strptime(date, date_format)
+    except ValueError:
+        return Error("404", "invalid date, date format is YYYY-MM-DD"), 404
+
+    c = CurrencyRates()
+    return Conversion(date, frm, to, 1.0, c.get_rate(frm, to, dt))
 
 
-def get_get_rates_from(_from, date=None):  # noqa: E501
+def get_get_rates_frm(frm, date=None):  # noqa: E501
     """Get all rates against a currency.
 
     Gets Array of Conversion of all available currencies against the base currency (from parameter). # noqa: E501
 
-    :param _from: Base currency to get rates against.
-    :type _from: str
+    :param _frm: Base currency to get rates against.
+    :type _frm: str
     :param date: YYYY-MM-DD format of date to check rates on.
     :type date: str
 
