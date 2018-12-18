5fabc1525bf1d8d3434ae43265877fe2ec758164
ibeckermayer <ibeckermayer@gmail.com>
2018-12-17 20:58:50 -0800

    made it actually use the PORT variable...

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -14,7 +14,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Currency Converter'})
-    app.run(port=8080)
+    app.run(port=PORT)
 
 
 if __name__ == '__main__':
