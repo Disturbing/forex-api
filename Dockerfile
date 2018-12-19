16202b88e94ee12a1d9cb92d532cce5aea3f0f4a
ibeckermayer <ibeckermayer@gmail.com>
2018-12-18 20:17:40 -0800

    updated so host is also specified by environment variable

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.6.4
 
 ARG FOREX_PORT
 ENV FOREX_PORT=${FOREX_PORT}
+ARG FOREX_HOST
+ENV FOREX_HOST=${FOREX_HOST}
 
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -9,12 +9,17 @@ try:
 except KeyError:
     raise ValueError("Must have environment variable FOREX_PORT defined to run this server")
 
+try:
+    HOST = os.environ["FOREX_HOST"]
+except KeyError:
+    raise ValueError("Must have environment variable FOREX_HOST defined to run this server")
+
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Currency Converter'})
-    app.run(port=PORT)
+    app.run(host=HOST, port=PORT)
 
 
 if __name__ == '__main__':
